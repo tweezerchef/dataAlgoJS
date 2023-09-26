e49290332bd729708797bc8d5296daeadbad845d
tweezerchef <ltomblock@gmail.com>
2023-09-26 13:31:26 -0500

    hisSumZeroWhileLoopO(n)

---
+++ sumZeroWhile.js
@@ -0,0 +1,18 @@
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]]
+        }
+        else if(sum > 0 ){
+            right--
+        }
+        else{
+            left++
+        }
+    }
+    return "not"
+}
+console.log(sumZero([-4, -3, -2, -1.5, 0, 1, 5, 10]))
