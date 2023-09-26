d76e16ba5e228ee380b448b9a12b0b2aafe91dd1
tweezerchef <ltomblock@gmail.com>
2023-09-26 13:02:48 -0500

    begeningOfSumZero

---
+++ sumZero.js
@@ -0,0 +1,9 @@
+// write a function sumZero which finds the first pair in a sorted array
+//and returns them as an array with 0(n) time complexity return "not" if not
+//found
+
+function sumZero(numArray){
+
+}
+
+sumZero([-4,-2,-2,-1,0,1,2,5])
