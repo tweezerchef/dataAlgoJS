c6f77a439541740d725c8bf7da164f0694c8d5c9
tweezerchef <ltomblock@gmail.com>
2023-09-26 13:22:56 -0500

    recursiveSolutionHellYeah

--- sumZero.js
+++ sumZero.js
@@ -2,8 +2,25 @@
 //and returns them as an array with 0(n) time complexity return "not" if not
 //found
 
-function sumZero(numArray){
-
+function sumZero(numArray, point1 = 0, point2 = numArray.length -1){
+    //establish pointers
+//if we have completed array sum not found return 'not'
+if(numArray[point1] >= 0){
+    return "Not"
+}
+//if sum 0 found return number array
+if(numArray[point1] + numArray[point2] === 0){
+    return [numArray[point1], numArray[point2]]
+}
+if(numArray[point1] + numArray[point2] >= 0){
+    point2--
+return sumZero(numArray,point1, point2)
+}
+if(numArray[point1] + numArray[point2] <= 0){
+    point1++
+return sumZero(numArray, point1, point2)
+}
+sumZero(numArray, point1, point2)
 }
 
-sumZero([-4,-2,-2,-1,0,1,2,5])
+console.log(sumZero([-4,-2,-2,-1,0,1,2,5]))
